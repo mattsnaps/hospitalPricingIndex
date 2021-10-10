82d024604538a70bc23bd90c31bf7bc66d283159
Matthew Priebe <mbpriebe@madisoncollege.edu>
2021-10-09 23:43:20 -0500

    Getting sample SQL code put together.

---
+++ src/test/resources/sample_hospital_pricing_schema_hospital.sql
@@ -0,0 +1,4 @@
+insert into sample_hospital_pricing_schema.hospital (id, hospital_Name)
+values  (1, 'Monroe Clinic'),
+        (2, 'St. Mary''s Hospital'),
+        (3, 'St. Anges Hospital');
---
+++ src/test/resources/sample_hospital_pricing_schema_procedure.sql
@@ -0,0 +1,7 @@
+insert into sample_hospital_pricing_schema.procedure (id, hcpcs_code, code_desc, code_desc_english)
+values  (1, '76700', 'HC US ABDOMEN COMPLETE', 'Ultrasound, abdominal, real time with image documentation'),
+        (2, '78452', 'HT MUSCLE IMAGE SPECT, MULT', 'Myocardial perfusion imaging, tomographic (SPECT) (including attenuation correction, qualitative or quantitative wall motion, ejection fraction by first pass or gated technique, additional quantification, when performed)'),
+        (3, '80048', 'METABOLIC PANEL TOTAL CA', 'BASIC METABOLIC PANEL - Glucose, Urea Nitrogen (BUN), Creatinine, Sodium (Na), Potassium (K),
+                        Chloride (CL), Carbon Dioxide (CO2), Anion Gap, Calcium'),
+        (4, 'DRG806', 'VAGINAL DELIVERY WITHOUT STERILIZATION/D&C WITH CC', 'VAGINAL DELIVERY WITHOUT STERILIZATION/D&C WITH CC'),
+        (5, '93017', 'HC CARD STRESS TEST ROUTINE', 'Cardiovascular stress test using maximal or submaximal treadmill or bicycle exercise, continuous electrocardiographic monitoring, and/or pharmacological stress');
