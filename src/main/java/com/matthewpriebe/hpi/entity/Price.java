435f30d97a03396632d2f4826719eb553813d3dd
Matthew Priebe <mbpriebe@madisoncollege.edu>
2021-10-13 22:01:53 -0500

    updating Javadocs

--- src/main/java/com/matthewpriebe/hpi/entity/Price.java
+++ src/main/java/com/matthewpriebe/hpi/entity/Price.java
@@ -1,12 +1,15 @@
 package com.matthewpriebe.hpi.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+
 
 import javax.persistence.*;
 
+/**
+ * The type Price.
+ * Represents the price of one procedure from one hospital
+ */
 @Data
 @NoArgsConstructor
 
@@ -17,35 +20,67 @@ import javax.persistence.*;
                 joinColumns = @JoinColumn(name = "hospital_id")),
         @AssociationOverride(name = "pk.procedure",
                 joinColumns = @JoinColumn(name = "procedure_id")) })
+
 public class Price implements java.io.Serializable{
 
     @EmbeddedId
     private PriceId pk = new PriceId();
     private String price;
 
+    /**
+     * Instantiates a new Price.
+     *
+     * @param price the price
+     */
     public Price(String price) {
         this.price = price;
     }
 
+    /**
+     * Instantiates a new Price.
+     *
+     * @param price the price
+     * @param pk    the pk
+     */
     public Price(String price,PriceId pk) {
         this.price = price;
         this.pk = pk;
     }
 
+    /**
+     * Gets procedure key.
+     *
+     * @return the procedure
+     */
     @Transient
     public Procedure getProcedure() {
         return getPk().getProcedure();
     }
 
+    /**
+     * Sets procedure key.
+     *
+     * @param procedure the procedure
+     */
     public void setProcedure(Procedure procedure) {
         getPk().setProcedure(procedure);
     }
 
+    /**
+     * Gets hospital key.
+     *
+     * @return the hospital
+     */
     @Transient
     public Hospital getHospital() {
         return getPk().getHospital();
     }
 
+    /**
+     * Sets hospital key.
+     *
+     * @param hospital the hospital
+     */
     public void setHospital(Hospital hospital) {
         getPk().setHospital(hospital);
     }
